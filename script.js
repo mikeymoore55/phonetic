dc5aa007a76d8cc6f988dd730c23211ad5c67c50
Michael Moore <michaelcalvinmoore@gmail.com>
2023-09-14 19:47:08 -0700

    Set up html and styles for game section

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const startButton = document.getElementById('startbutton');
 const startUp = document.getElementById('startup');
+const game = document.getElementById('game');
 
 const beginGame = () => {
    startUp.style.display = 'none';
+   game.style.display = 'flex';
 }
 
 startButton.onclick = beginGame;
